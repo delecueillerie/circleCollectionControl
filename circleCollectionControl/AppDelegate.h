a93b000a4f73f47c0d719d02e5fec4b45c1b3706
Lagspoon <lagspoon@gmail.com>
2015-01-02 17:52:36 +0100

    Initial Commit

---
+++ circleCollectionControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  circleCollectionControl
+//
+//  Created by Olivier Delecueillerie on 02/01/2015.
+//  Copyright (c) 2015 lagspoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
